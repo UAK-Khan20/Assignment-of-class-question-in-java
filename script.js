ad6fcd7d8d7015b87ee38d755ae25bb21424dfc7
UAK-Khan20 <156125155+UAK-Khan20@users.noreply.github.com>
2024-01-24 22:03:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+                                  // ASSIGNMENT-1
+
+        // Question -1: 
+
+let age = 22; 
+let isStudent = true; 
+
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+        // Question -2:
+
+// window.onload = function() {
+//     alert("Welcome!\n\nThanks to visit from our website!.");
+// };
+
+                                   // ASSIGNMENT-2
+
+        // Question-1:
+
+// let length = prompt("Enter a length in cm ..");
+// let Width = prompt("Enter a Width in cm ..");
+// const Area = length * Width ;
+// console.log(Area+"cm")
+
+        // Question-2:
+
+// window.onload = function() {
+//     let userName = prompt("What is your name?");
+//     alert("Hello, " + userName + "!\n\nWelcome to our website.");
+// };
